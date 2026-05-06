c80cc9a05172f7b2f6e535a0c1a6700ca81c9b3d
Tim Hoehn <timothy.hoehn14@gmail.com>
2026-05-06 13:33:07 -0400

    Add deployment config for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 
@@ -33,4 +34,5 @@ def home():
     """
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
